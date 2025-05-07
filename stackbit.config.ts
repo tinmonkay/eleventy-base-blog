f60cf095e4d45eb86117f773784b71278763e87a
spereverde <spereverde@gmail.com>
2025-05-07 11:37:28 +0200

    add 11ty stackbit conf and git content source

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,46 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "eleventy",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+	stackbitVersion: "~0.6.0",
+	nodeVersion: "18",
+	ssgName: "eleventy",
+	contentSources: [
+		new GitContentSource({
+			rootPath: __dirname,
+			contentDirs: ["content"],
+			models: [
+				{
+					name: "Post",
+					type: "page",
+					urlPath: "/{slug}",
+					filePath: "content/blog/{slug}.json",
+					fields: [{ name: "title", type: "string", required: true }],
+				},
+			],
+			assetsConfig: {
+				referenceType: "static",
+				staticDir: "public",
+				uploadDir: "img",
+				publicPath: "/",
+			},
+		}),
+	],
+	postInstallCommand: "npm i --no-save @stackbit/types",
+
+	// Eleventy to run inside Visual Editor container
+	devCommand: "npx @11ty/eleventy --serve --port {PORT}",
+
+	// Eleventy-specific configuration
+	experimental: {
+		ssg: {
+			proxyWebsockets: true,
+			logPatterns: {
+				up: ["Server at"],
+			},
+		},
+	},
+
+	// Specific option to prevent Visual Editor from interfering with Eleventy's page reload mechanism
+	customContentReload: true,
+});
