5f2d4012200da491353a42c17c034cbf6a391b2a
tinmonkay <tinmonkay@gmail.com>
2025-05-07 12:26:15 +0200

    add sitemap

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,4 @@
-import { defineStackbitConfig } from "@stackbit/types";
+import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
@@ -26,6 +26,38 @@ export default defineStackbitConfig({
 			},
 		}),
 	],
+	siteMap: ({ documents, models }) => {
+		// 1. Filter all page models
+		const pageModels = models.filter((m) => m.type === "page");
+
+		return (
+			documents
+				// 2. Filter all documents which are of a page model
+				.filter((d) => pageModels.some((m) => m.name === d.modelName))
+				// 3. Map each document to a SiteMapEntry
+				.map((document) => {
+					// Map the model name to its corresponding URL
+					const urlModel = (() => {
+						switch (document.modelName) {
+							case "Page":
+								return "otherPage";
+							case "Blog":
+								return "otherBlog";
+							default:
+								return null;
+						}
+					})();
+
+					return {
+						stableId: document.id,
+						urlPath: `/${urlModel}/${document.id}`,
+						document,
+						isHomePage: false,
+					};
+				})
+				.filter(Boolean) as SiteMapEntry[]
+		);
+	},
 	postInstallCommand: "npm i --no-save @stackbit/types",
 
 	// Eleventy to run inside Visual Editor container
